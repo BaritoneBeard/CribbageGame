c1fa1cd9f6ad2fa63dbc48ba92137db5299b4c57
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-09-30 16:04:24 -0400

    Created client.py, returns request status of localhost:5000

--- client.py
+++ client.py
@@ -3,9 +3,16 @@ import requests
 
 def main():
     os.environ['NO_PROXY'] = '127.0.0.1'  # disable proxy or else request cannot find localhost
-    r = requests.get('http://127.0.0.1:5000')
+    try:
+        r = requests.get('http://127.0.0.1:5000')
+    except ConnectionError:
+        print("The server you are trying to reach is down. Try again later.")
+        r = 500
+    except:
+        print("Something went wrong")
+        r = 500
 
-    # when server is up, returns 200
+    # when server is up, returns 200. When down
     print(r)
 
 
