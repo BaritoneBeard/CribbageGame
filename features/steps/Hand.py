04936be568c9aea3bf804f1129ad027e6112455c
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-11-01 19:27:51 -0400

    added comments to make tests a little more readable

--- features/steps/Hand.py
+++ features/steps/Hand.py
@@ -5,6 +5,8 @@ from behave import *
 
 url = 'http://127.0.0.1:5000'
 
+#### have the server up before testing ###
+
 @given('I can create my hand')
 def step_impl(context):
     # data for the hand
@@ -12,19 +14,18 @@ def step_impl(context):
     hand = { 'data':json.dumps(cards) }
     r = requests.post(url + '/hand', data= hand)
     context.response = r.status_code
-    assert context.response == 201
+    assert context.response == 201  # checks if this worked successfully
 
 @then('I can see my hand')
 def step_impl(context):
     r = requests.get(url + '/hand')
-    hand = json.loads(r.text)
+    hand = json.loads(r.text)  # comes back as a string, we want a list for assertion purposes
     hand = hand['hand']
     hand = hand.replace('"', '')
     hand = hand.replace('[', '')
     hand = hand.replace(']', '')
     hand = list(hand.split(","))
     hand = [hand.strip(' ') for hand in hand]
-    print(hand[3])
     assert hand is not None
     assert hand[0] == "AC"
     assert hand[3] == "9S"
