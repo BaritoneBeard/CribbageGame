4c1c29241a5f1b97b8d5a49469bf9ea457dfe0f4
BaritoneBeard <NickAlmeder@Yahoo.com>
2021-11-24 19:19:04 -0500

    Cleaned up test file for scoring

--- features/steps/scoring_feat.py
+++ features/steps/scoring_feat.py
@@ -1,20 +1,16 @@
 from behave import *
 from scoring import calc_15
 
-'''Feature: I can tally up the score of a player's hand.
-  Scenario: I can tally up the score of a player's hand
-    Given I have a list of card ranks
-    Then I can tally how many combinations total fifteen'''
-
-url = 'http://pcms.game-host.org:8543/'
-deckname = 'decks/tdn'
-card_ranks = [4,5,5,6,10]
-
 @given('I have a list of card ranks')
 def step_impl(context):
+    card_ranks = [4, 5, 5, 6, 10]
     assert len(card_ranks) != 0, "the list is non empty"
 
 @then('I can tally how many combinations total fifteen')
 def step_impl(context):
     card_ranks = [4, 5, 5, 6, 10]
     assert calc_15(card_ranks) == 4     # 4,5,6 is 15, 5,10 is 15, that's it: 2+2 points = 4 points.
+    card_ranks = [3,5,7,9,11]
+    assert calc_15(card_ranks) == 2     # 3,5,7 = 15. That's it. 2 points.
+    card_ranks = []
+    assert calc_15(card_ranks) == 0     # no cards, returns 0. May need some error handling in-function
