2d92bb16ba291085d43f4b8784d9739b50243afb
Tyler <tmartell5656@gmail.com>
2021-12-03 14:04:17 -0500

    Added some fill-in stuff for a Player class for testing purposes. Created post testing method for player.

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import os  # is this included in base python? attempting to install throws error
 import requests
 import logging
 import json
+from Hand import Hand
 
 logger = logging.getLogger("client.py")
 logging.basicConfig(filename="clientlog.txt")
@@ -17,10 +18,12 @@ def test_game_resource_post(game_obj, game_ID):
     post_request = requests.post(url=URL, data=DATA)
     print(post_request.text)
 
+
 def test_game_resource_get(game_ID):
     URL = url + 'games/' + str(game_ID)
     get_request = requests.get(url=URL)
     print(get_request.text)
+
 
 def test_game_resource_delete(game_ID):
     URL = url + 'games/' + str(game_ID)
@@ -28,13 +31,28 @@ def test_game_resource_delete(game_ID):
     print(delete_request.text)
 
 
+def test_player_resource_post(player_obj, game_ID):
+    URL = url + 'games/' + str(game_ID) + '/' + player_obj.player_name  # specify the game_ID the player is a part of.
+    DATA = {'player': player_obj}
+    player_post_request = requests.post(url=URL, data=DATA)
+    print(player_post_request.text)
+
+
+
+
 
 
 def testing_grounds():
-    game = Game(123)
-    test_game_resource_post(game, game.game_ID)
-    test_game_resource_get(game.game_ID) # Access (get) a game by passing in that game's ID number.
-    test_game_resource_delete(game.game_ID)
+    sample_game = Game(123)
+    test_game_resource_post(sample_game, sample_game.game_ID)
+    test_game_resource_get(sample_game.game_ID) # Access (get) a sample_game by passing in that sample_game's ID number.
+    test_game_resource_delete(sample_game.game_ID)
+
+    print()
+
+    sample_hand = Hand(['2H', 'KD'], ['AS', '7C'])
+    sample_player = Player(sample_hand, True, False, 'tyler')
+    test_player_resource_post(sample_player, sample_game.game_ID)
 
 
 class Game:
@@ -43,8 +61,14 @@ class Game:
         self.player_2 = Player
         self.game_ID = game_ID
 
+
 class Player:
-    pass
+    def __init__(self, hand, turn, crib_turn, name):
+        self.hand = hand
+        self.score = 0
+        self.turn = turn
+        self.crib_turn = crib_turn
+        self.player_name = name
 
 
 testing_grounds()
