442f33e5f3a8e3579f3ae739de03f424e5b92194
Tyler <tmartell5656@gmail.com>
2021-12-03 13:24:54 -0500

    Fixed testing method for creating a game. The data passed into the API is now a Game object. Now, the game_ID is used as a key to access a particular Game.

--- client.py
+++ client.py
@@ -10,51 +10,26 @@ logger.setLevel(logging.INFO)
 
 url = 'http://127.0.0.1:5000'
 
-'''
-def main():
-    os.environ['NO_PROXY'] = '127.0.0.1'  # disable proxy or else request cannot find localhost
 
-    # data for the hand
-    # DEPRECATED : still here because it's data that gets sent
-    cards = ["AC", "7D", "KH", "9S"]
-    hand = { 'data':json.dumps(cards) }
-
-    #parameters = {'data':"buy a new computer"}
-
-    logger.info("attempting to connect with server . . .")
-    try:
-        r = requests.get(url + '/hand')
-        logger.info("Successfully posted data to server")
-
-    except ConnectionError:
-        print("The server you are trying to reach is down. Try again later.")
-        r = 500
-        logger.error("Connection Error")
-    except TypeError:
-        print("Given parameters cannot be requested")
-        r = 404
-        logger.error("Type Error")
-    except:
-        print("Something went wrong")
-        r = 500
-        logger.error("Connection failed due to some unexpected error")
-
-    print(r)
-    print(r.text)
-    print(r.status_code)
-    
-'''
-
-def test_game_resource_post(game_ID):
+def test_game_resource_post(game_obj, game_ID):
     URL = url + '/games/' + str(game_ID)
-    DATA = {'game_ID': game_ID}
+    DATA = {'game_ID': game_obj}
     post_request = requests.post(url=URL, data=DATA)
     print(post_request.text)
 
+def test_game_resource_get(game_ID):
+    pass
 
 
-test_game_resource_post(1234)
+def testing_grounds():
+    game = Game(123)
+    test_game_resource_post(game, game.game_ID)
 
-# if __name__ == '__main__':
-#     #test_game_resource_post()
-#     main()
+class Game:
+    def __init__(self, game_ID):
+        self.player_1 = Player
+        self.player_2 = Player
+        self.game_ID = game_ID
+
+class Player:
+    pass
